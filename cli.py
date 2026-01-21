179d9ae11b13cb994d4be2ab24e4c5144fcee264
ttammann <t@tammann.com>
2026-01-21 10:52:55 -0600

    Add tapper and charge current to cli.py

--- cli.py
+++ cli.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
 
     # mw.set_voltage(3.50 * 16)
     # mw.set_float_voltage(3.375 * 16)
+    # mw.set_taper_current(4)
+    # mw.set_charge_current(12)
     # mw.set_restart_voltage(3.1 * 16)
+
 
     mw.shutdown()
